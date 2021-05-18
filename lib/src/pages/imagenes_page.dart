a4225b2e9560ee40975e236c4329f029f867a0d6
FlutLab.io <>
2021-05-18 23:34:51 +0000

    Autosave

--- lib/src/pages/imagenes_page.dart
+++ lib/src/pages/imagenes_page.dart
@@ -13,10 +13,10 @@ class ImagenesPage extends StatelessWidget {
       body: Container(
         child: Container(
         child: Image.asset('assets/app.gif'),
-        
+        child: Image.asset('assets/programador.gif')
         
       ),
-        child: Image.asset('assets/programador.gif')
+        
       ),//Fin Body
     );
   }
