58407608f192c3a2523fbbd410d0b57bc3e350bb
FlutLab.io <>
2021-05-18 23:51:50 +0000

    Autosave

--- lib/src/pages/imagenes_page.dart
+++ lib/src/pages/imagenes_page.dart
@@ -6,31 +6,31 @@ class ImagenesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text('Gifs', style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.pink,
+        title: Text('Imagenes'),
       ),
-      body: new GridView.extent(
-        maxCrossAxisExtent: 300.0,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        padding: const EdgeInsets.all(5),
-        children: _buildGridTiles(3),
-      ), //Fin Gridview
-    ); //Fin Scaffold
-  } //Fin Widget
-} //Fin Pgina Principal State
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+ 
+              // Image(
+              //   image: NetworkImage('https://hipertextual.com/files/2019/09/hipertextual-the-legend-of-zelda-links-awakening-2019999870.jpg'),
+              // ),
+ 
+              FadeInImage(
+                
+                image: NetworkImage('https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/vacunas-consejos-peru.jpg'),
+                image: NetworkImage('https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/4.jpeg'),
+ 
+              )
 
-List<Widget> _buildGridTiles(numberOfTiles) {
-  List<Container> containers = new List<Container>.generate(numberOfTiles, (int index) {
-    //index = 0, 1, 2,...
-    final imageName = index < 9 ? 'assets/image0${index + 1}.gif' : 'assets/image${index + 1}.gif';
-    return new Container(
-      child: new Image.asset(imageName, fit: BoxFit.fill),
-    );
-  }); //Fin Contenedor generar nombre de foto
-  return containers;
-} //Fin de List
+
+
+ 
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
