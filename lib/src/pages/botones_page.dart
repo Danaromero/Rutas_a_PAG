ef108143d97267d5265ca73cf3c1aec03d72bada
FlutLab.io <>
2021-05-18 22:38:12 +0000

    Autosave

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -29,7 +29,7 @@ class BotonesPage extends StatelessWidget {
                 child: Text('Soy un Boton con Bordes'),
                 onPressed: () {},
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                color: Colors.white24,
+                color: Colors.white70,
                 textColor: Colors.black,
               ),
               FloatingActionButton(
