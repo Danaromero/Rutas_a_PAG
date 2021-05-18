fca1297b8897ea4b357de81615cac0d430ef999a
FlutLab.io <>
2021-05-18 21:35:55 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -60,7 +60,7 @@ class HomePage extends StatelessWidget {
 
 
 
-            title: Text(nombre),
+            title: Text(nombre, style: TextStyle(fontFamily: 'RobotoMono'),),
             trailing: Icon(Icons.launch_outlined   , color: Colors.teal),
           );
   }//Fin Widget _crearItem creada por el usuario
