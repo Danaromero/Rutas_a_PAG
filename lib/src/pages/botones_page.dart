836dd18d563cc98a1cfa292c6aa4ac0958cee51b
FlutLab.io <>
2021-05-18 22:13:05 +0000

    Autosave

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -22,6 +22,28 @@ class BotonesPage extends StatelessWidget {
   onPressed: () { },
   child: Text('FlatButton with custom overlay colors'),
 ),
+TextButton(
+  style: ButtonStyle(
+    overlayColor: MaterialStateProperty.resolveWith<Color>(
+      (Set<MaterialState> states) {
+        if (states.contains(MaterialState.focused))
+          return Colors.red;
+        if (states.contains(MaterialState.hovered))
+            return Colors.green;
+        if (states.contains(MaterialState.pressed))
+            return Colors.blue;
+        return null; // Defer to the widget's default.
+    }),
+  ),
+  onPressed: () { },
+  child: Text('TextButton with custom overlay colors'),
+),
+
+
+
+
+
+
 
               FlatButton(
                 onPressed: () {
