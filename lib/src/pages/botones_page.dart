16875df9016383b28707d7609a545269d1aa9cfd
FlutLab.io <>
2021-05-18 22:25:55 +0000

    Autosave

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -25,28 +25,14 @@ class BotonesPage extends StatelessWidget {
               ),
 
 
-RaisedButton(
-        textColor: Colors.white,
-        padding: EdgeInsets.all(0.0),
-        shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-        onPressed: () {},
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[
-                  Color(0xFF0D47A1),
-                  Color(0xFF1976D2),
-                  Color(0xFF42A5F5),
-                ],
+FlatButton(
+                child: Text('Rounded Rectangle Border'),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                color: Colors.pink,
+                textColor: Colors.white,
               ),
-              borderRadius: BorderRadius.all(Radius.circular(80.0))
-          ),
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-          child: Text(
-              'Click Me',
-              style: TextStyle(fontSize: 20,)
-          ),
-        )
 
 
 
