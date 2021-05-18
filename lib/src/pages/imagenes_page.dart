acbb0f62f3bb24804f1df70e5082e262454f8728
FlutLab.io <>
2021-05-18 23:27:06 +0000

    Autosave

--- lib/src/pages/imagenes_page.dart
+++ lib/src/pages/imagenes_page.dart
@@ -13,15 +13,8 @@ class ImagenesPage extends StatelessWidget {
           child: Column(
             children: <Widget>[
  
-              // Image(
-              //   image: NetworkImage('https://hipertextual.com/files/2019/09/hipertextual-the-legend-of-zelda-links-awakening-2019999870.jpg'),
-              // ),
- 
-              FadeInImage(
-                placeholder: AssetImage('assets/loading.gif'),
-               
- 
-              )
+              margin: const EdgeInsets.all(4),
+        child: Image.asset('assets/progra.gif'),
 
 
 
