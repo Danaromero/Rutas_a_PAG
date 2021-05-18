61b0d33974c6cf141bbe5b6764486822a7a88861
FlutLab.io <>
2021-05-18 21:22:00 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,6 +14,7 @@ class HomePage extends StatelessWidget {
     
       body: ListView(
         children: <Widget>[
+          Divider(),
  
           _crearItem(context, Icons.widgets, 'Botones', 'botones'),
           Divider(),
