8d8d75a88785d38048b7285900086d3aba2ee469
FlutLab.io <>
2021-05-19 21:59:55 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Widgets App Romero', style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.teal,
       ),
       body: ListView(
         children: <Widget>[
