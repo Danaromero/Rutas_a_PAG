0edf9fc04e5575271145d5372223b0f88bd5bfda
FlutLab.io <>
2021-05-19 23:42:44 +0000

    Autosave

--- lib/src/pages/cards_page.dart
+++ lib/src/pages/cards_page.dart
@@ -57,7 +57,7 @@ class CardsPage extends StatelessWidget {
  
     return Card(
       elevation: 5,
-      color: Colors.amber,
+      color: Colors.yellow,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
  
       child: Container(
