b47b609092a3786f7500535b78dda6d3d258689f
FlutLab.io <>
2021-05-19 20:51:21 +0000

    Autosave

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -14,6 +14,7 @@ class ListViewPage extends StatelessWidget {
         backgroundColor: Colors.pink,
       ),
         body: ListView(
+          padding: EdgeInsets.all(20),
           children: <Widget>[
             Container(
               height: 50,
