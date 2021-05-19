9ed46826301293b95a41eab94657b01d730fe587
FlutLab.io <>
2021-05-19 15:30:09 +0000

    Autosave

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -5,26 +5,16 @@ class AlertPage extends StatelessWidget {
   final GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
  
   @override
-  Widget _buildAlertDialog() {
-    return AlertDialog(
-      title: Text('Notificaciones'),
-      content:
-          Text("¿Desea recibir notificaciones? Serán muy pocas de verdad :)"),
-      actions: [
-        FlatButton(
-            child: Text("Aceptar"),
-            textColor: Colors.blue,
-            onPressed: () {
-              Navigator.of(context).pop();
-            }),
-
-        FlatButton(
-            child: Text("Cancelar"),
-            textColor: Colors.red,
-            onPressed: () {
-              Navigator.of(context).pop();
-            }),
-      ],
-    );
-  }
+  @widget
+Widget homeView(BuildContext context, String title) => Scaffold(
+    body: Center(
+        child: MaterialButton(
+  color: Colors.yellow,
+  child: Text('Show Alert'),
+  onPressed: () {
+    // Show a basic widget
+    Alert(context: context, title: "FilledStacks", desc: "Flutter is awesome.")
+        .show();
+  },
+)));
 }
