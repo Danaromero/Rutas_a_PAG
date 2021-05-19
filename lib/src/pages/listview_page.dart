3c1a8fd79af7c6fbdcbdd5274fd47a47f8047d54
FlutLab.io <>
2021-05-19 20:43:27 +0000

    Autosave

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -3,12 +3,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class ListViewPage extends StatefulWidget {
-  @override
-  _ListViewPageState createState() => _ListViewPageState();
-}
 
-class _ListViewPageState extends StatelessWidget {
+class ListViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
