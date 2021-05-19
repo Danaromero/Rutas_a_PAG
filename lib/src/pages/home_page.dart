145184d71113458327ec09aa6130b2991b6d6361
FlutLab.io <>
2021-05-19 20:12:47 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: Text('Dana Romero Widgets App',   style: TextStyle(fontWeight: FontWeight.bold),
 ),
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.amber,
       ),
     
       body: ListView(
