ab672748fbc8dcf63bd52fdae3a72d43e5608b56
FlutLab.io <>
2021-05-19 23:12:01 +0000

    Autosave

--- lib/src/pages/imagenes_page.dart
+++ lib/src/pages/imagenes_page.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
- 
+
 class ImagenesPage extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Imagenes', style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Colors.indigo,
       ),
       body: Center(
-        
         child: Container(
           child: Column(
-            padding: EdgeInsets.all(20),
             children: <Widget>[
               FadeInImage(
                 placeholder: AssetImage('assets/loading.gif'),
                 image: NetworkImage('https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/vacunas-consejos-peru.jpg'),
-              
               ),
-              
               FadeInImage(
                 placeholder: AssetImage('assets/loading.gif'),
-                image: NetworkImage('https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/vacunas-consejos-peru.jpg'),
+                image: NetworkImage('https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/6.jpeg'),
               ),
               FadeInImage(
                 placeholder: AssetImage('assets/loading.gif'),
