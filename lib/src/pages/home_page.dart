f52765f3241502d13d12458a3fadb3e818d77bed
FlutLab.io <>
2021-05-19 20:20:21 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,9 +7,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       theme: new ThemeData(
         primarySwatch: Colors.lightGreen,
-        primaryColor: const Color(0xFFdca5a5),
-        accentColor: const Color(0xFFff2020),
-        canvasColor: const Color(0xFFfafafa),
+       
         ),
       appBar: AppBar(
         centerTitle: true,
