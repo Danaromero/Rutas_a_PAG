1f6ad01d1da1453f13ac77633c182530b50b8fe0
FlutLab.io <>
2021-05-19 22:40:41 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
           color: Colors.black,
           decoration: TextDecoration.underline,
          
-          decorationStyle: TextDecorationStyle.double,
+          decorationStyle: TextDecorationStyle.solid,
         ),
         ),
         backgroundColor: Colors.black12,
