3d5d30a175d828b99a21c899aa05c42f0b90d0bb
FlutLab.io <>
2021-05-19 14:21:40 +0000

    Autosave

--- lib/src/pages/circle_page.dart
+++ lib/src/pages/circle_page.dart
@@ -13,12 +13,12 @@ class CirclePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
  
-            _crearContacto(context),
-            _crearContacto(context),
-            _crearContacto(context),
-            _crearContacto(context),
-            _crearContacto(context),
-             _crearContacto(context)
+            _crearContacto1(context),
+            _crearContacto2(context),
+            _crearContacto3(context),
+            _crearContacto4(context),
+            _crearContacto5(context),
+             _crearContacto6(context)
  
           ],
         ),
@@ -26,7 +26,7 @@ class CirclePage extends StatelessWidget {
     );
   }
  
- Widget  _crearContacto(BuildContext context) {
+ Widget  _crearContacto1(BuildContext context) {
  
    return Row(
      children: <Widget>[
@@ -61,5 +61,200 @@ class CirclePage extends StatelessWidget {
      ],
    );
  
- }
-}
+ }//Fin Widget Crearcontacto1
+
+Widget  _crearContacto2(BuildContext context) {
+ 
+   return Row(
+     children: <Widget>[
+ 
+       Container(
+         height: 100,
+         width: 100,
+         decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+         fit: BoxFit.cover,
+        image: AssetImage('assets/camara.png')
+ ),
+),
+
+
+       ),
+ 
+       Padding(
+         padding: EdgeInsets.all(8),
+         child: Column(
+           children: <Widget>[
+ 
+             Text('837363763736', style: Theme.of(context).textTheme.subtitle1,),
+             SizedBox(height: 5,),
+             Text('Gerardo Ramos', style: Theme.of(context).textTheme.subtitle2)
+ 
+           ],
+         ),
+       )
+ 
+     ],
+   );
+ 
+ }//Fin Crear Contacto 2
+
+Widget  _crearContacto3(BuildContext context) {
+ 
+   return Row(
+     children: <Widget>[
+ 
+       Container(
+         height: 100,
+         width: 100,
+         decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+         fit: BoxFit.cover,
+        image: AssetImage('assets/camara.png')
+ ),
+),
+
+
+       ),
+ 
+       Padding(
+         padding: EdgeInsets.all(8),
+         child: Column(
+           children: <Widget>[
+ 
+             Text('837363763736', style: Theme.of(context).textTheme.subtitle1,),
+             SizedBox(height: 5,),
+             Text('Gerardo Ramos', style: Theme.of(context).textTheme.subtitle2)
+ 
+           ],
+         ),
+       )
+ 
+     ],
+   );
+ 
+ }//Fin Crear Contacto 3
+
+Widget  _crearContacto4(BuildContext context) {
+ 
+   return Row(
+     children: <Widget>[
+ 
+       Container(
+         height: 100,
+         width: 100,
+         decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+         fit: BoxFit.cover,
+        image: AssetImage('assets/camara.png')
+ ),
+),
+
+
+       ),
+ 
+       Padding(
+         padding: EdgeInsets.all(8),
+         child: Column(
+           children: <Widget>[
+ 
+             Text('837363763736', style: Theme.of(context).textTheme.subtitle1,),
+             SizedBox(height: 5,),
+             Text('Gerardo Ramos', style: Theme.of(context).textTheme.subtitle2)
+ 
+           ],
+         ),
+       )
+ 
+     ],
+   );
+ 
+ }//Fin Crear Contacto 4
+
+
+
+ Widget  _crearContacto5(BuildContext context) {
+ 
+   return Row(
+     children: <Widget>[
+ 
+       Container(
+         height: 100,
+         width: 100,
+         decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+         fit: BoxFit.cover,
+        image: AssetImage('assets/camara.png')
+ ),
+),
+
+
+       ),
+ 
+       Padding(
+         padding: EdgeInsets.all(8),
+         child: Column(
+           children: <Widget>[
+ 
+             Text('837363763736', style: Theme.of(context).textTheme.subtitle1,),
+             SizedBox(height: 5,),
+             Text('Gerardo Ramos', style: Theme.of(context).textTheme.subtitle2)
+ 
+           ],
+         ),
+       )
+ 
+     ],
+   );
+ 
+ }//Fin Crear Contacto 5
+
+
+ Widget  _crearContacto6(BuildContext context) {
+ 
+   return Row(
+     children: <Widget>[
+ 
+       Container(
+         height: 100,
+         width: 100,
+         decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+         fit: BoxFit.cover,
+        image: AssetImage('assets/camara.png')
+ ),
+),
+
+
+       ),
+ 
+       Padding(
+         padding: EdgeInsets.all(8),
+         child: Column(
+           children: <Widget>[
+ 
+             Text('837363763736', style: Theme.of(context).textTheme.subtitle1,),
+             SizedBox(height: 5,),
+             Text('Gerardo Ramos', style: Theme.of(context).textTheme.subtitle2)
+ 
+           ],
+         ),
+       )
+ 
+     ],
+   );
+ 
+ }//FIn crear contacto 6
+
+
+
+
+
+
+
+}//Fin Clase Circle Page
