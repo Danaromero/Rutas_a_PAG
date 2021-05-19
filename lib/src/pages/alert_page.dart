70a7d61f1c1fb56dee0e7058921350cc5883d9e2
FlutLab.io <>
2021-05-19 15:38:08 +0000

    Autosave

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -4,115 +4,50 @@ class AlertPage extends StatelessWidget {
  
   final GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
  
+ int _counter = 0;
+  String textValue = '?';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: key,
       appBar: AppBar(
-        title: Text('Alertas'),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
- 
-            FlatButton(
-              onPressed: () {
- 
-                showDialog(
-                  
-                  context: context,
-                  builder: (_) => mostrarAlerta2(context)
-                  
-                  );
-              },
-              child: Text('Mostar Alerta'),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-              color: Colors.amberAccent,
-            ),
- 
-             FlatButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-              color: Colors.blue,
-              onPressed: () {
- 
-                key.currentState.showSnackBar(
- 
-                  SnackBar(
-                    content: Text('Hola, soy una Snacbar'),
-                  )
- 
-                );
-              },
-              child: Text('Mostar Snackbar'),
-            )
-
-
-
-
- 
-          ],
-        ),
+          child: Text(textValue)
       ),
-    );
-  }
- 
-  Widget mostrarAlerta(BuildContext context) {
- 
-    return AlertDialog(
-      title: Text('Hola', style: TextStyle(color: Colors.white),),
-      content: Text('Soy una alerta',style: TextStyle(color: Colors.white)),
-      actions: <Widget>[
- 
-        FlatButton(
-          onPressed: () {
- 
-            Navigator.pop(context);
-          },
-          child: Text('Cancelar'),
- 
-        ),
- 
-        FlatButton(
-    
-          onPressed: () {
- 
-               Navigator.pop(context);
-          },
-          child: Text('Okay!'),
-          
-        )
- 
-      ],
-    );
-  }
- 
-  Widget mostrarAlerta2(BuildContext context) {
- 
-    return showPlatformDialog(
-  context: context,
-  builder: (_) => BasicDialogAlert(
-    title: Text("Select account"),
-    content: Container(
-      height: 200,
-      child: ListView(
-        children: <Widget>[
-          _buildListSampleItem("contact@jdg.ph"),
-          _buildListSampleItem("hello@gmail.com"),
-          _buildListSampleItem("hi@joshuadeguzman.net"),
-          _buildListSampleItem("jdeguzman@freelancer.com"),
-        ],
-      ),
-    ),
-    actions: <Widget>[
-      BasicDialogAction(
-        title: Text("Cancel"),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pop(context);
+          _showDialog(context);
         },
+        child: Icon(Icons.add),
       ),
-    ],
-  ),
-);
+    );
   }
+
+  _showDialog(BuildContext ctx) {
+    showDialog(context: ctx,
+        builder: (context) {
+          return AlertDialog(
+              title: Center(
+                  child:Text(
+                      "Alerta!!!")),
+              content: TextField(
+                  decoration: InputDecoration(
+                    hintText: textValue,
+                  ),
+                  onSubmitted: (text){
+                    setState(() {
+                      textValue=text;
+                    });
+
+                    Navigator.pop(ctx);
+                  }));
+        }
+    );
+  }
+
+}
+
+
 }
