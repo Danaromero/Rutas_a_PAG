e44185cd235f788f091b38c64a05c85c6baba432
FlutLab.io <>
2021-05-20 00:10:40 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -62,7 +62,7 @@ class HomePage extends StatelessWidget {
              tileColor: Colors.white10,
         selectedTileColor: Colors.greenAccent,
 
-            leading: Icon(icon, color: Colors.black),
+            leading: Icon(icon, color: Colors.indigo),
 
 
 
